314f8799513687a3fafa455c3e20ea12691bd374
Paul Merrill <pdm@pdm.me>
2017-09-21 17:10:44 -0700

    schema.sql: Add confirmations, analyses tables
    Based on the discussion from Sep 21st, 2017.

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 DROP TABLE IF EXISTS schema_version;
 CREATE TABLE schema_version (
     last_modified INTEGER
@@ -13,7 +15,9 @@ DROP TABLE IF EXISTS passwords;
 CREATE TABLE passwords (
     user_id INTEGER PRIMARY KEY,
     salt TEXT NOT NULL,
-    password_hash TEXT NOT NULL
+    password_hash TEXT NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES  users (id) ON DELETE CASCADE
 );
 
 DROP TABLE IF EXISTS logs;
@@ -21,8 +25,24 @@ CREATE TABLE logs (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     filename TEXT NOT NULL,
-    blob TEXT NOT NULL
+    blob TEXT NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES  users (id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS confirmations;
+CREATE TABLE confirmations (
+    token TEXT PRIMARY KEY,
+    is_error INTEGER NOT NULL  -- 0 for no, 1 for yes
+);
+
+DROP TABLE IF EXISTS analyses;
+CREATE TABLE analyses (
+    log_id INTEGER PRIMARY KEY,
+    analysis_json TEXT NOT NULL,
+
+    FOREIGN KEY (log_id) REFERENCES  logs (id) ON DELETE CASCADE
 );
 
 DROP INDEX IF EXISTS users_email;
-CREATE INDEX users_email ON users (email);
+CREATE UNIQUE INDEX users_email ON users (email);
