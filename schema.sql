b9ddd52369297f39b14931baf2cace886495c3a7
Paul Merrill <pdm@pdm.me>
2017-10-06 21:29:07 -0700

    schema.sql: Separate logs from log_contents

--- schema.sql
+++ schema.sql
@@ -11,6 +11,9 @@ CREATE TABLE users (
     email TEXT UNIQUE NOT NULL
 );
 
+DROP INDEX IF EXISTS users_email;
+CREATE UNIQUE INDEX users_email ON users (email);
+
 DROP TABLE IF EXISTS passwords;
 CREATE TABLE passwords (
     user_id INTEGER PRIMARY KEY,
@@ -20,15 +23,40 @@ CREATE TABLE passwords (
     FOREIGN KEY (user_id) REFERENCES  users (id) ON DELETE CASCADE
 );
 
+DROP TABLE IF EXISTS log_contents;
+CREATE TABLE log_contents (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    blob TEXT NOT NULL,
+    hash TEXT NOT NULL
+);
+
+DROP INDEX IF EXISTS log_contents_hash;
+CREATE INDEX log_contents_hash ON log_contents (hash);
+
 DROP TABLE IF EXISTS logs;
 CREATE TABLE logs (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     filename TEXT NOT NULL,
-    blob TEXT NOT NULL,
+    log_content_id INTEGER NOT NULL,
 
-    FOREIGN KEY (user_id) REFERENCES  users (id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (log_content_id) REFERENCES log_contents (id) ON DELETE CASCADE
 );
+
+DROP INDEX IF EXISTS log_log_content_id;
+CREATE INDEX log_log_content_id ON logs (log_content_id);
+
+-- When the last log that refers to a log_contents is deleted, also delete that log_contents.
+DROP TRIGGER IF EXISTS delete_log_contents;
+CREATE TRIGGER delete_log_contents AFTER DELETE ON logs
+BEGIN
+    DELETE FROM log_contents
+    WHERE old.log_content_id = log_contents.id AND
+          (SELECT COUNT(*)
+           FROM logs
+           WHERE logs.log_content_id = log_contents.id) = 0;
+END;
 
 DROP TABLE IF EXISTS confirmations;
 CREATE TABLE confirmations (
@@ -36,13 +64,18 @@ CREATE TABLE confirmations (
     is_error INTEGER NOT NULL  -- 0 for no, 1 for yes
 );
 
+--INSERT INTO confirmations (token, is_error) VALUES ('AUTHORIZATION_FAILURE', 1);
+--INSERT INTO confirmations (token, is_error) VALUES ('AUTHORIZATION_FAILURE', 1);
+--INSERT INTO confirmations (token, is_error) VALUES ('AUTHORIZATION_FAILURE', 1);
+--INSERT INTO confirmations (token, is_error) VALUES ('AUTHORIZATION_FAILURE', 1);
+--INSERT INTO confirmations (token, is_error) VALUES ('AUTHORIZATION_FAILURE', 1);
+--INSERT INTO confirmations (token, is_error) VALUES ('AUTHORIZATION_FAILURE', 1);
+--INSERT INTO confirmations (token, is_error) VALUES ('AUTHORIZATION_FAILURE', 1);
+
 DROP TABLE IF EXISTS analyses;
 CREATE TABLE analyses (
-    log_id INTEGER PRIMARY KEY,
+    log_content_id INTEGER PRIMARY KEY,
     analysis_json TEXT NOT NULL,
 
-    FOREIGN KEY (log_id) REFERENCES  logs (id) ON DELETE CASCADE
+    FOREIGN KEY (log_content_id) REFERENCES log_content (id) ON DELETE CASCADE
 );
-
-DROP INDEX IF EXISTS users_email;
-CREATE UNIQUE INDEX users_email ON users (email);
