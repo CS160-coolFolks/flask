2f5616bf81d8c8998bd31d90e4f36111d0d2475f
Paul Merrill <pdm@pdm.me>
2017-10-17 11:58:00 -0700

    starter_confirmations.sql: Add some known non-errors

--- starter_confirmations.sql
+++ starter_confirmations.sql
@@ -1,7 +1,4 @@
 INSERT INTO confirmations (token, is_error) VALUES ('AUTHORIZATION_FAILURE', 1);
---INSERT INTO confirmations (token, is_error) VALUES ('AUTHORIZATION_FAILURE', 1);
---INSERT INTO confirmations (token, is_error) VALUES ('AUTHORIZATION_FAILURE', 1);
---INSERT INTO confirmations (token, is_error) VALUES ('AUTHORIZATION_FAILURE', 1);
---INSERT INTO confirmations (token, is_error) VALUES ('AUTHORIZATION_FAILURE', 1);
---INSERT INTO confirmations (token, is_error) VALUES ('AUTHORIZATION_FAILURE', 1);
---INSERT INTO confirmations (token, is_error) VALUES ('AUTHORIZATION_FAILURE', 1);
+INSERT INTO confirmations (token, is_error) VALUES ('0  connect failure', 0);
+INSERT INTO confirmations (token, is_error) VALUES ('0  transport failure', 0);
+INSERT INTO confirmations (token, is_error) VALUES ('0  closed abnormally', 0);
